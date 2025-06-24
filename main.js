fcb623d66692ce2b7dd3ae0514e162aa7053d957
zall-ofc <zallofc7@gmail.com>
2025-06-24 14:28:45 +0700

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const icon = document.getElementById('icon')
-const listMenu = document.getElementById('list-menu')
-
-icon.addEventListener('click', () => {
-    listMenu.classList.toggle('hidden');
-});
